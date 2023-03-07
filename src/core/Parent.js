6f01d9f97314ede26bcdf4bbe61f410fd1dd3673
Richard Dobroň <office.etexweb@gmail.com>
2023-03-07 17:01:11 +0100

    :nail_care: Improve Parent.js

--- src/core/Parent.js
+++ src/core/Parent.js
@@ -1,9 +1,38 @@
+import CSSCore from "fbjs/lib/CSSCore";
+
 export function byTag(node, tagName) {
   tagName = tagName.toUpperCase();
+  node = find(node, function (el) {
+    return el.nodeName === tagName;
+  });
 
-  while (node && node.nodeName !== tagName) {
+  return node instanceof Element ? node : null;
+}
+
+export function byClass(node, className) {
+  node = find(node, function (el) {
+    return el instanceof Element && CSSCore.hasClass(el, className);
+  });
+
+  return node instanceof Element ? node : null;
+}
+
+export function byAttribute(node, attribute) {
+  node = find(node, function (el) {
+    return el instanceof Element && el.hasAttribute(attribute);
+  });
+
+  return node instanceof Element ? node : null;
+}
+
+export function find(node, callback) {
+  while (node) {
+    if (callback(node)) {
+      return node;
+    }
+
     node = node.parentNode;
   }
 
-  return node;
+  return null;
 }
